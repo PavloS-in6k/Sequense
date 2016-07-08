8dd8ac77ca53141d6e818ed563048ac853f06e9e
Pavlo-in6k <pavlos@interlink-ua.com>
2016-07-08 16:00:28 +0300

    Finished task with not optimized algorithm

--- src/com/in6k/Sequense/SequenseFinder.java
+++ src/com/in6k/Sequense/SequenseFinder.java
@@ -1,10 +1,40 @@
 package com.in6k.Sequense;
 
+import java.util.ArrayList;
+
 /**
  * Created by employee on 7/8/16.
  */
 public class SequenseFinder {
-    public static int[] getMaxSequense(int[] array) {
-        return new int[0];
+    public static int getSum(ArrayList<Integer> subsequense)
+    {
+        int summ = 0;
+        for (int i = 0; i < subsequense.size(); i++) {
+            summ+=subsequense.get(i);
+        }
+        return summ;
+    }
+
+    public static ArrayList<Integer> getMaxSequense(ArrayList<Integer> fullSequense) {
+        if (fullSequense==null)
+            return new ArrayList<Integer>();
+        if (fullSequense.size()==1)
+            return fullSequense;
+        ArrayList<Integer> sequense = new ArrayList<Integer>();
+
+        int temporarySumm = Integer.MIN_VALUE;
+
+        for (int i = 0; i < fullSequense.size(); i++) {
+            for (int k = fullSequense.size(); k > i; k--)
+            {
+                if (getSum(new ArrayList<Integer>(fullSequense.subList(i,k)))>temporarySumm) {
+                    temporarySumm = getSum(new ArrayList<Integer>(fullSequense.subList(i, k)));
+                    sequense = new ArrayList<Integer>(fullSequense.subList(i, k));
+                }
+
+            }
+        }
+
+        return sequense;
     }
 }
--- test/com/in6k/Sequense/SequenseFinderTests.java
+++ test/com/in6k/Sequense/SequenseFinderTests.java
@@ -3,13 +3,97 @@ package com.in6k.Sequense;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertArrayEquals;
+
 /**
  * Created by employee on 7/8/16.
  */
 public class SequenseFinderTests {
+
     @Test
     public void nullArray() throws Exception {
-        Assert.assertArrayEquals(SequenseFinder.getMaxSequense(null), new int[0]);
+        assertArrayEquals(SequenseFinder.getMaxSequense(null).toArray(), new ArrayList<Integer>().toArray());
+    }
 
+    @Test
+    public void emptyArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>()).toArray(),
+                new ArrayList<Integer>().toArray());
+    }
+
+    @Test
+    public void oneElementArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense
+                (new ArrayList<Integer>(Arrays.asList(1))).toArray(), new ArrayList<Integer>(Arrays.asList(1)).toArray());
+    }
+
+    @Test
+    public void oneNegativeElementArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(-1))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(-1)).toArray());
+    }
+
+    @Test
+    public void twoElementsArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(1, 2))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(1, 2)).toArray());
+    }
+
+    @Test
+    public void firstPositiveSecondNegativeArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(2, -5))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(2)).toArray());
+    }
+
+    @Test
+    public void firstNegativeSecondPositiveArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(-5, 2))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(2)).toArray());
+    }
+
+    @Test
+    public void bothNegativeAndEqualArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(-5, -5))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(-5)).toArray());
+    }
+
+    @Test
+    public void bothNegativeAndNonEqualArray() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(-4, -7))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(-4)).toArray());
+    }
+
+    @Test
+    public void sequenseAtTheBeggining() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(15, 3, -18, 9, -50, 17))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(15, 3)).toArray());
+    }
+
+    @Test
+    public void sequenseAtTheEnd() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(17, -18, 90, -50, 90))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(90, -50, 90)).toArray());
+    }
+
+    @Test
+    public void sequenseAtTheMiddle() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(13, -15, 17, -100, 3))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(17)).toArray());
+    }
+
+    @Test
+    public void inversionLadderSequense() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(4, -5, 7, -8, 10, -11, 13))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(13)).toArray());
+
+    }
+
+    @Test
+    public void theSmallestNegative() throws Exception {
+        assertArrayEquals(SequenseFinder.getMaxSequense(new ArrayList<Integer>(Arrays.asList(-14, -13, -6, -5, -1, -3))).toArray(),
+                new ArrayList<Integer>(Arrays.asList(-1)).toArray());
     }
 }
